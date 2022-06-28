d697801eee712aadc269e58fa47486c28e5fae84
Pyxels <39232833+Pyxels@users.noreply.github.com>
2022-06-28 13:35:47 +0200

    feat(voteroom): add variables and data struct to contract

---
+++ ethereum/contracts/VoteRoom.sol
@@ -0,0 +1,36 @@
+// // SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.8.0;
+
+/// @title Vote room smart contract, a proof-of-concept voting system with private and public voting
+/// @author Group 3 (Jonas Buse, Emir Simsek, Felix Pollok, Michael Meissner)
+/// @notice A smart contract that lets managers create new votes which either designated adresses can partake in, or which everybody can use
+contract VoteRoom {
+
+    /// A single vote which can be public or private
+    struct VoteData {
+        string description;
+        bool isFinalized;
+        mapping(address=>bool) hasVoted;
+        uint inFavor;
+        uint againt;
+        uint abstain;
+        uint minimumVotes;
+        bool isPublic;
+    }
+
+    /// Owner and manager who can whitelist people and start votes
+    address public manager;
+
+    /// Mapping of all whitelisted voters
+    mapping(address=>bool) public invitedVoters;
+
+    /// Number of whitelisted voters
+    uint public voterCount;
+
+    /// Array of votes
+    VoteData[] public votes;
+
+    /// Description of the room
+    string public voteRoomDescription;
+
+}
