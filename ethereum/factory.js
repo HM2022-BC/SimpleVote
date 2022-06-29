98009c938d1c1e7e8c123ca1c9e6407d3884d576
Pyxels <39232833+Pyxels@users.noreply.github.com>
2022-06-29 13:33:38 +0200

    feat(backend): add function to return whole array of created voterooms

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import RoomCreator from './build/contracts/RoomCreator.json';
 
-const roomCreatorAddress = "0x693d413208831C55bdD2a6221C811feDaC0098B7";
+const roomCreatorAddress = "0xDA84c102c60e9419505C4ABd7ca1794BA2eE9620";
 
 const instance = new web3.eth.Contract(RoomCreator.abi, roomCreatorAddress);
 
