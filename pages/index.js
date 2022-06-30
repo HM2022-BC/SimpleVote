3064e65f20074d6636d6892a633d9345d8b8c056
Pyxels <39232833+Pyxels@users.noreply.github.com>
2022-06-30 13:00:18 +0200

    feat(frontend): extended rooms overview to show more data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, Button } from 'semantic-ui-react';
+import VoteRoom from '../ethereum/voteRoom';
 import factory from '../ethereum/factory';
 import Layout from '../components/Layout';
 import { Link } from '../routes';
@@ -7,24 +8,45 @@ import { Link } from '../routes';
 class VoteRoomOverview extends Component {
   static async getInitialProps() {
     const voteRooms = await factory.methods.getAllVoteRooms().call();
+    const voteRoomsWithDetails = await Promise.all(
+      voteRooms.map(async address => {
+        const voteRoom = VoteRoom(address);
+        const description = await voteRoom.methods.voteRoomDescription().call();
+        const voterCount = await voteRoom.methods.voterCount().call();
+        const manager = await voteRoom.methods.manager().call();
+        const voteNumber = await voteRoom.methods.getNumberOfVotes().call();
+        return { address, description, voterCount, manager, voteNumber };
+      })
+    );
 
-    return { voteRooms };
+    return { voteRoomsWithDetails };
   }
 
   renderVoteRooms() {
-    const items = this.props.voteRooms.map(address => {
-      return {
-        header: address,
-        description: (
-          <Link route={`/voteroom/${address}`}>
-            <a>View Room</a>
-          </Link>
-        ),
-        fluid: true
-      };
+    return this.props.voteRoomsWithDetails.map(({ address, description, voterCount, voteNumber }) => {
+      return <Card raised={true} style={{ minWidth: '600px' }
+      } centered={true} >
+        <Card.Content>
+          <Card.Header textAlign='center'>{address}</Card.Header>
+          <Card.Description textAlign='center'>{description}</Card.Description>
+          <Card.Meta textAlign='center'>{voterCount}👤   |   {voteNumber} 🗳️</Card.Meta>
+        </Card.Content>
+        <Card.Content extra >
+          <div className="ui two buttons" style={{ justifyContent: 'center' }}>
+            <Link route={`/voteroom/${address}`} >
+              <a style={{ marginRight: '20px' }}>
+                <Button primary >Show Room</Button>
+              </a>
+            </Link>
+            <Link route={`/voteroom/${address}/votes`}>
+              <a style={{ marginLeft: '20px' }}>
+                <Button primary floated='right'>View Votes</Button>
+              </a>
+            </Link>
+          </div>
+        </Card.Content>
+      </Card >;
     });
-
-    return <Card.Group items={items} />;
   }
 
   render() {
@@ -44,7 +66,9 @@ class VoteRoomOverview extends Component {
             </a>
           </Link>
 
-          {this.renderVoteRooms()}
+          <Card.Group>
+            {this.renderVoteRooms()}
+          </Card.Group>
         </div>
       </Layout>
     );
