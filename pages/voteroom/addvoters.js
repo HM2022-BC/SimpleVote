1f4dac3bd599b86752206a53be7dd11409ac4b16
Pyxels <39232833+Pyxels@users.noreply.github.com>
2022-07-01 15:12:28 +0200

    refactor(frontend): switched from class component to functional component in new voter

--- pages/voteroom/addvoters.js
+++ pages/voteroom/addvoters.js
@@ -1,76 +1,74 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Form, Button, Message, Input, Checkbox } from 'semantic-ui-react';
 import VoteRoom from '../../ethereum/voteRoom';
 import web3 from '../../ethereum/web3';
 import { Link, Router } from '../../routes';
 import Layout from '../../components/Layout';
 
-class InviteVoter extends Component {
-  state = {
+const InviteVoter = (props) => {
+  const [state, setState] = React.useState({
+    address: '',
     newVoters: [],
-    errorMessage: ''
-  };
+    errorMessage: '',
+  });
 
-  static async getInitialProps(props) {
-    const { address } = props.query;
+  React.useEffect(() => {
+    setState({ ...state, address: props.url.query.address });
+  }, []);
 
-    return { address };
-  }
-
-  onSubmit = async event => {
+  const onSubmit = async event => {
     event.preventDefault();
-    this.setState({ loading: true, errorMessage: '' });
+    setState({ ...state, loading: true, errorMessage: '' });
 
-    const voteRoom = VoteRoom(this.props.address);
+    const voteRoom = VoteRoom(state.address);
 
     try {
-      this.state.newVoters.forEach((address) => {
+      state.newVoters.forEach((address) => {
         if (!web3.utils.isAddress(address.trim())) {
           throw new Error(`Invalid address: ${address}`);
         }
       })
       const accounts = await web3.eth.requestAccounts();
       await voteRoom.methods
-        .inviteVoters(this.state.newVoters.map(e => e.trim()))
+        .inviteVoters(state.newVoters.map(e => e.trim()))
         .send({ from: accounts[0] });
 
-      Router.pushRoute(`/voteroom/${this.props.address}`);
+      Router.pushRoute(`/voteroom/${state.address}`);
     } catch (err) {
-      this.setState({ errorMessage: err.message });
+      console.error(err);
+      setState({ ...state, errorMessage: err.message });
     }
 
-    this.setState({ loading: false });
+    setState({ ...state, loading: false });
   };
 
-  render() {
-    return (
-      <Layout>
-        <Link route={`/voteroom/${this.props.address}/`}>
-          <a>Back</a>
-        </Link>
-        <h3>Add new Voters</h3>
-        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
-          <Form.Field>
-            <label>Invite Voters</label>
-            <div style={{ fontSize: '10px', color: 'gray' }}>Comma Separated</div>
-            <Input
-              value={this.state.newVoters}
-              onChange={event =>
-                this.setState({ newVoters: event.target.value.split(',') })}
-              label="Addresses"
-              labelPosition="left"
-              placeholder="0x... , 0x..."
-            />
-          </Form.Field>
+  return (
+    <Layout>
+      <Link route={`/voteroom/${state.address}/`}>
+        <a>Back</a>
+      </Link>
+      <h3>Add new Voters</h3>
+      <Form onSubmit={onSubmit} error={!!state.errorMessage}>
+        <Form.Field>
+          <label>Invite Voters</label>
+          <div style={{ fontSize: '10px', color: 'gray' }}>Comma Separated</div>
+          <Input
+            value={state.newVoters}
+            onChange={event =>
+              setState({ newVoters: event.target.value.split(',') })}
+            label="Addresses"
+            labelPosition="left"
+            placeholder="0x... , 0x..."
+          />
+        </Form.Field>
 
-          <Message error header="Oops!" content={this.state.errorMessage} />
-          <Button primary loading={this.state.loading}>
-            Invite!
-          </Button>
-        </Form>
-      </Layout>
-    );
-  }
+        <Message error header="Oops!" content={state.errorMessage} />
+        <Button primary loading={state.loading}>
+          Invite!
+        </Button>
+      </Form>
+    </Layout>
+  );
 }
 
 export default InviteVoter;
